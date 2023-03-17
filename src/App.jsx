83d5dc1c3472ca21264b3fc3d20fe320038aad81
audemusprime <edbrossa@gmail.com>
2023-03-17 10:19:34 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+/* 
+Page Section components:
+- Header
+- Page arrow
+- Landing
+- About
+- Services
+- Testimonials
+- Contact
+- Footer
+
+Subsection components AND/OR requirements:
+LANDING
+  - Hero image
+ABOUT
+  - Owner description
+    - Photo
+    - Description
+SERVICES
+  - Fee list x3
+  - Jump to contact button
+TESTIMONIALS
+  - Image carousel
+  - Testimonial carousel OR section??
+CONTACT
+  - Contact form
+  - Newsletter subscription
+  - Phone numbers
+*/
+
+function App() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js, jsx}'],
+  theme: {
+    extend: {
+      fontFamily: [],
+    },
+  },
+  plugins: [],
+};
+/* 
+
+TODO -  Determine fonts for:
+- Nav bar logo
+- Headers
+- Subheaders
+- Body
+- Testimonials
+
+*/
