2af6b0a0966f478adc51b17661f50d9d6aa8f34d
audemusprime <edbrossa@gmail.com>
2023-03-17 10:26:03 -0400

    fills out components, App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
+import Landing from './components/Landing';
+import Arrow from './components/Arrow';
+import About from './components/About';
+import Services from './components/Services';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 /* 
 Page Section components:
@@ -33,7 +41,15 @@ CONTACT
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <Header />
+        <Landing />
+        <About />
+        <Services />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </div>
     </>
   );
 }
