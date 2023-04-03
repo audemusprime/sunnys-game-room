d07c886dfd325302abb7e6d86450281893b85425
audemusprime <edbrossa@gmail.com>
2023-04-03 17:40:27 -0400

    adds fade in animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,56 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import '../css/style.css';
 
 function About() {
+  const options = {
+    rootMargin: '0px',
+    threshold: 0.15
+  };
+
+  const [
+    isIntersecting,
+    setIsIntersecting
+  ] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    }, options);
+    console.log(isIntersecting);
+    observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, [isIntersecting]);
+
+  useEffect(() => {
+    if (isIntersecting) {
+      ref.current.classList.add('fadeIn');
+    }
+  }, [isIntersecting]);
+
   return (
-    <section className="page subpage about">
-      <div id="about-header">
-        <h2 className="subpage-header" id="who-we-are">
-          Who we are
-        </h2>
-      </div>
-      <div id="sunny">
-        <img src="https://picsum.photos/250" alt="" />
-      </div>
-      <div id="desc-a">
-        Words about Sunny. Words about Sunny. Words about Sunny. Words about Sunny. Words about Sunny. Words about
-        Sunny. Words about Sunny. Words about Sunny.
-      </div>
-      <div id="desc-b">
-        Words about Dylan. Words about Dylan. Words about Dylan. Words about Dylan. Words about Dylan. Words about
-        Dylan. Words about Dylan. Words about Dylan.
-      </div>
-      <div id="dylan">
-        <img src="https://picsum.photos/250" alt="" />
-      </div>
-    </section>
+    <>
+      <section className="page subpage about" id="who-we-are" ref={ref}>
+        <div id="about-header">
+          <h2 className="subpage-header">Who we are</h2>
+        </div>
+        <div id="sunny">
+          <img src="https://picsum.photos/250" alt="" />
+        </div>
+        <div id="desc-a">
+          Words about Sunny. Words about Sunny. Words about Sunny. Words about Sunny. Words about Sunny. Words about
+          Sunny. Words about Sunny. Words about Sunny.
+        </div>
+        <div id="desc-b">
+          Words about Dylan. Words about Dylan. Words about Dylan. Words about Dylan. Words about Dylan. Words about
+          Dylan. Words about Dylan. Words about Dylan.
+        </div>
+        <div id="dylan">
+          <img src="https://picsum.photos/250" alt="" />
+        </div>
+      </section>
+    </>
   );
 }
 
