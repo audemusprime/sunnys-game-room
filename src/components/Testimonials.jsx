f207332d565e0105ba10ff5f6228887707620a12
audemusprime <edbrossa@gmail.com>
2023-04-18 15:49:49 -0400

    adds review carousel

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import reviews from '../data/reviews';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 import '../css/style.css';
 
 function Testimonials() {
@@ -26,9 +28,60 @@ function Testimonials() {
       ref.current.classList.add('fadeIn');
     }
   }, [isIntersecting]);
+
+  const reviewData = reviews.map((review) => (
+    <div key={review.name}>
+      <p id="review-text">{review.review}</p>
+      <br />
+      <span id="review-name">{review.name}</span> &nbsp; &nbsp;-&nbsp;&nbsp; {review.rating}
+      <br />
+    </div>
+  ));
+
+  const arrowStyles = {
+    position: 'absolute',
+    zIndex: 2,
+    top: 'calc(50% - 15px)',
+    width: 30,
+    height: 30,
+    cursor: 'pointer',
+    color: 'var(--body-background)',
+    opacity: 0.5,
+    filter: 'none'
+  };
+
   return (
-    <section className="page subpage" id="what-people-said" ref={ref}>
-      <h2 className="subpage-header">What people said</h2>
+    <section className="page subpage testimonials" id="what-people-said" ref={ref}>
+      <div>
+        <h2 className="subpage-header">What people said</h2>
+      </div>
+      <div className="image-carousel">testing out this area</div>
+      <div className="review-carousel">
+        <h2 id="contact-header">Reviews</h2>
+        <Carousel
+          showArrows={true}
+          autoPlay={true}
+          infiniteLoop={true}
+          interval={6000}
+          showStatus={false}
+          showIndicators={false}
+          renderArrowPrev={(onClickHandler, hasPrev) =>
+            hasPrev && (
+              <button onClick={onClickHandler} style={{ ...arrowStyles, left: 5 }}>
+                ◀︎
+              </button>
+            )
+          }
+          renderArrowNext={(onClickHandler, hasPrev) =>
+            hasPrev && (
+              <button onClick={onClickHandler} style={{ ...arrowStyles, right: 5 }}>
+                ▶︎
+              </button>
+            )
+          }>
+          {reviewData}
+        </Carousel>
+      </div>
     </section>
   );
 }
